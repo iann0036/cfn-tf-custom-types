4fa0e2697be66a620e02f8b37e90a3b317355fb7
Ian Mckay <ian.mckay@kablamo.com.au>
2021-04-20 14:32:23 +1000

    Explicit print

--- generate.py
+++ generate.py
@@ -308,7 +308,7 @@ def process_provider(provider_type):
     print("Downloading latest {} provider version...".format(provider_type))
     exec_call(['terraform', 'init'], tempdir.absolute())
     tfschemadata = exec_call(['terraform', 'providers', 'schema', '-json'], tempdir.absolute())
-    print(tfschemadata)
+    print("##" + tfschemadata + "$$")
     tfschema = json.loads(tfschemadata)
 
     exec_call(['git', 'clone', provider_data["data"][0]["attributes"]["source"], provider_type], tempdir.absolute())
